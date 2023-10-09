00f39b7a320991e1216909f284386d3db4a35c12
Salisu Shuaibu <salisshuaibu11@gmail.com>
2023-10-09 17:42:27 +0100

    Deposit event

--- contracts/BankAccount.sol
+++ contracts/BankAccount.sol
@@ -1,5 +1,10 @@
 pragma solidity >=0.4.22 <=0.8.19;
 
 contract BankAccount {
-  
+  event Deposit(
+    address indexed user,
+    uint indexed accountId,
+    uint value,
+    uint timestamp
+  );
 }
