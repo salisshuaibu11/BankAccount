a962305bf5f57885b79efc8bea36ea49845608cc
Salisu Shuaibu <salisshuaibu11@gmail.com>
2023-10-10 11:21:16 +0100

    it("Should allow creating a single user account")

--- test/BankAccount.js
+++ test/BankAccount.js
@@ -6,17 +6,27 @@ const { expect } = require("chai");
 
 describe("BankAccount", function () {
   async function deployBankAccount() {
-    const [addr0, addr1, addr2, addr3] = await ethers.getSigners();
+    const [addr0, addr1, addr2, addr3, addr4] = await ethers.getSigners();
 
     const BankAccount = await ethers.getContractFactory("BankAccount");
-    const backAccount = await BankAccount.deploy(unlockTime, { value: lockedAmount });
+    const bankAccount = await BankAccount.deploy();
 
-    return { backAccount, addr0, addr1, addr2, addr3 };
+    return { bankAccount, addr0, addr1, addr2, addr3, addr4 };
   }
 
   describe("Deployment", () => {
     it("Should deploy without error", async () => {
       await loadFixture(deployBankAccount);
     });
-  })
+  });
+
+  describe('Creating an account', () => { 
+    it("Should allow creating a single user account", async() => {
+      const { bankAccount, addr0 } = await loadFixture(deployBankAccount); 
+      await bankAccount.connect(addr0).createAccount([]);
+
+      const accounts = await bankAccount.connect(addr0).getAccounts();
+      expect(accounts.length).to.equal(1);
+    });
+  });
 });
