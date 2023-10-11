4a28296113e05f2257ef5e8efd7bbe408b2b177e
Salisu Shuaibu <salisshuaibu11@gmail.com>
2023-10-11 20:25:12 +0100

    it("Should not allow creating an account with 5 owners")

--- test/BankAccount.js
+++ test/BankAccount.js
@@ -71,9 +71,23 @@ describe("BankAccount", function () {
       expect(accounts4.length).to.equal(1);
     });
 
-    it("Should allow not allow creating an account with duplicate owners", async() => {
+    it("Should not allow creating an account with duplicate owners", async() => {
       const { bankAccount, addr0 } = await loadFixture(deployBankAccount); 
       await expect(bankAccount.connect(addr0).createAccount([addr0.address]))
+        .to.reverted;
+    });
+
+    it("Should not allow creating an account with 5 owners", async() => {
+      const { bankAccount, addr0, addr1, addr2, addr3, addr4 } = await loadFixture(deployBankAccount); 
+      await expect(bankAccount.connect(addr0).createAccount(
+        [
+          addr0.address, 
+          addr1.address,
+          addr2.address,
+          addr3.address,
+          addr4.address
+        ]
+      ))
         .to.reverted;
     });
   });
